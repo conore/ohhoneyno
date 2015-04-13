db00e70d9cd6d2a4331bbbdcd12616fe55263d02
CONOR EGAN <conoregan@CONORs-MacBook-Air.local>
2015-04-13 11:39:05 -0700

    Initial Prototype Commit

---
+++ Assets/_Scripts/Bag.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bag : MonoBehaviour {
+
+	public float moveSpeed = 200.0f;
+	public float bounceMultiplier = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate() {
+
+		Vector3 vel = new Vector3 ();
+		
+		if (Input.GetAxis ("Horizontal") != 0) {
+			vel.x = (Input.GetAxis ("Horizontal")*Time.deltaTime*moveSpeed);
+		}
+
+		GetComponent<Rigidbody>().velocity = vel;
+
+		if (Input.GetKeyDown ("return")) {
+			vel = new Vector3();
+			vel.y = (bounceMultiplier*Time.deltaTime*moveSpeed);
+			GetComponent<Rigidbody>().velocity = vel;
+
+			GameObject.Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/_Scripts/OhHoneyNo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OhHoneyNo : MonoBehaviour {
+
+	public GameObject itemPrefab;
+	public float spawnHeight = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//determine spawn point (heiht above bag)
+		Vector3 spawnPoint;
+		GameObject bag = GameObject.FindGameObjectWithTag ("bag");
+
+		if (bag != null) {
+			spawnPoint = bag.transform.position;
+			spawnPoint.y += spawnHeight;
+
+			//add some randomness to the spawn point
+			float rand = Random.Range(-.5f,.5f);
+
+			spawnPoint.x -= rand;
+			spawnPoint.z += rand;
+
+			if (Input.GetKeyDown ("space")) {
+				GameObject.Instantiate (itemPrefab, spawnPoint, Quaternion.identity);
+			}
+		}
+	}
+}
