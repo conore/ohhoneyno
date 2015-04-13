2a0e0e0bd26baf003790c09d8f1cdcb31d9729e4
James Devane <james@Jamess-MacBook-Pro.local>
2015-04-13 15:55:39 -0700

    adding floor file and sound

---
+++ Assets/_Scripts/Floor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+
+	public AudioClip floorCollide;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+			
+		AudioSource.PlayClipAtPoint (floorCollide, gameObject.transform.position);
+		
+	}
+}
